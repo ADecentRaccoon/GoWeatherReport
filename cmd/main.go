41be6c39e4ccf0faf7cfeb21373b7b2534068a30
ADecentRaccoon <vovamanuev@gmail.com>
2025-04-20 20:47:27 +0300

    merged addDocker branch. Created .env.example file

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
-import(
+import (
+	"fmt"
+	"os"
+
 	"github.com/ADecentRaccoon/GoWeatherReport/routers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -12,6 +15,8 @@ func main() {
 	r := gin.Default()
 	weatherGroup := r.Group("/weather")
 	routers.WeatherRouter(weatherGroup)
-
-	r.Run("localhost:8080")
+	HOST := os.Getenv("RUNNING_AT_HOST")
+	PORT := os.Getenv("RUNNING_AT_PORT")
+	fmt.Println("Running at", HOST, ":", PORT)
+	r.Run(fmt.Sprintf("%v:%v", HOST, PORT))
 }
--- utils/redis.go
+++ utils/redis.go
@@ -13,7 +13,7 @@ import (
 var redisContext = context.Background()
 
 func SaveResponse(location string, dataToSave any){
-	var RedisClient = redis.NewClient(&redis.Options{Addr: fmt.Sprintf("%v:%v", os.Getenv("DB_HOST"), os.Getenv("REDIS_PORT"))})
+	var RedisClient = redis.NewClient(&redis.Options{Addr: fmt.Sprintf("%v:%v", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))})
 	redisStatus := RedisClient.Set(redisContext, location, dataToSave, 5 * time.Minute)
 	if redisStatus.Err() != nil{
 		fmt.Println("redisError: ", redisStatus.Err().Error())
@@ -21,11 +21,12 @@ func SaveResponse(location string, dataToSave any){
 }
 
 func GetResponse(location string) (bool, []byte) {
-	var RedisClient = redis.NewClient(&redis.Options{Addr: fmt.Sprintf("%v:%v", os.Getenv("DB_HOST"), os.Getenv("REDIS_PORT"))})
+	var RedisClient = redis.NewClient(&redis.Options{Addr: fmt.Sprintf("%v:%v", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))})
 	ans, err := RedisClient.Get(redisContext, location).Result()
 	if err == redis.Nil {
 		return false, nil
 	} else if err != nil {
+		fmt.Println(err)
 		return false, nil
 	}
 
